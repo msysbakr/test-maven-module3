67d14804cab2f5bb3fe002c77867617e40b8d567
Abu Bakar Siddique <bakr@medisys.com.sa>
2015-08-04 13:02:44 +0600

    04AUGUST2015 13:02

--- src/main/java/test/maven/maven1/test/maven/module3/App.java
+++ src/main/java/test/maven/maven1/test/maven/module3/App.java
@@ -5,6 +5,6 @@ import test.maven.maven1.test_maven_module2.App2;
 public class App {
 	public static void main(String[] args) {
 		String print = new App2().test(".....DONE!");
-		System.out.println("PRINT::" + print);
+		System.out.println("PRINT::" + print + " ==>> printing complete.");
 	}
 }
